2504cc05bea39db53b777393eead7bf630de0181
DanielLott4 <54151017+DanielLott4@users.noreply.github.com>
2023-11-15 09:48:01 -0700

    Squashed commit of the following:
    commit c4bd7cefca31d7937be30b4ddae0d56faffacb45
    Author: DanielLott4 <54151017+DanielLott4@users.noreply.github.com>
    Date:   Wed Nov 15 09:47:09 2023 -0700
    
        working
    
    commit 16406241dd2202dad232272c876ce5ad8174b317
    Author: DanielLott4 <54151017+DanielLott4@users.noreply.github.com>
    Date:   Wed Nov 15 09:40:21 2023 -0700
    
        push1

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ import { Pool, OkPacket } from 'mysql2/promise';
 
 
 const app = express();
-app.use(bodyParser.json()); 
+app.use(bodyParser.json());
 
 app.use(express.static(path.join('dist', 'server', 'public_html')));
 
@@ -17,130 +17,163 @@ let dbPool: Pool;
 
 // Connect to MySQL database
 connectDB()
-  .then(pool => {
-    dbPool = pool;
-  })
-  .catch(error => {
-    console.error(`Database connection error: ${error}`);
-  });
+	.then(pool => {
+		dbPool = pool;
+	})
+	.catch(error => {
+		console.error(`Database connection error: ${error}`);
+	});
 
 
 // creates new todo
 // test with: "curl -X POST -H "Content-Type: application/json" -d '{"taskName": "testname1", "taskInfo": "testinfo", "isCompleted": false, "deadline": "2023-10-23"}' "http://localhost:3000/api/todo""
-app.post('/api/todo', async (req: Request, res: Response) => {
-  const { taskName, taskInfo, isCompleted, deadline } = req.body;
+app.post('/api/lists/:listId/tasks', async (req: Request, res: Response) => {
+	const { listId } = req.params;
+	const { taskName, taskInfo, isCompleted, deadline } = req.body;
 
-  // Data validation
-  if (!taskName || typeof taskName !== 'string') {
-    return res.status(400).json({ error: 'Invalid input' });
-  }
+	// Data validation for listId and other fields
+	if (!listId || isNaN(parseInt(listId))) {
+		return res.status(400).json({ error: 'Invalid list ID' });
+	}
 
-  try {
-    const [result] = await dbPool.query('INSERT INTO sys.Tasks (taskName, taskInfo, isCompleted, deadline) VALUES (?, ?, ?, ?)', [taskName, taskInfo, isCompleted, deadline]);
-    const okPacket = result as OkPacket; 
-    return res.status(201).json({ message: 'To-Do item created', id: okPacket.insertId });
-  } catch (err) {
-    console.error(err);
-    return res.status(500).json({ error: 'Database error' });
-  }  
+	if (!taskName || typeof taskName !== 'string') {
+		return res.status(400).json({ error: 'Invalid input' });
+	}
+
+	try {
+		const [result] = await dbPool.query(
+			'INSERT INTO sys.Tasks (listId, taskName, taskInfo, isCompleted, deadline) VALUES (?, ?, ?, ?, ?)',
+			[listId, taskName, taskInfo, isCompleted, deadline]
+		);
+		const okPacket = result as OkPacket;
+		return res.status(201).json({ message: 'To-Do item created', id: okPacket.insertId });
+	} catch (err) {
+		console.error(err);
+		return res.status(500).json({ error: 'Database error' });
+	}
 });
 
 // create a new list
 app.post('/api/lists', async (req: Request, res: Response) => {
-  const { listName } = req.body; 
+	const { listName } = req.body;
 
-  // Data validation
-  if (!listName || typeof listName !== 'string') {
-    return res.status(400).json({ error: 'Invalid list name' });
-  }
+	// Data validation
+	if (!listName || typeof listName !== 'string') {
+		return res.status(400).json({ error: 'Invalid list name' });
+	}
 
-  try {
-    // Insert the new list into the database
-    const [result] = await dbPool.query('INSERT INTO sys.Lists (listName) VALUES (?)', [listName]);
-    const okPacket = result as OkPacket;
-    
-    // Respond with the ID of the newly created list
-    return res.status(201).json({ message: 'List created', id: okPacket.insertId });
-  } catch (err) {
-    console.error(err);
-    return res.status(500).json({ error: 'Database error' });
-  }
+	try {
+		// Insert the new list into the database
+		const [result] = await dbPool.query('INSERT INTO sys.Lists (listName) VALUES (?)', [listName]);
+		const okPacket = result as OkPacket;
+
+		// Respond with the ID of the newly created list
+		return res.status(201).json({ message: 'List created', id: okPacket.insertId });
+	} catch (err) {
+		console.error(err);
+		return res.status(500).json({ error: 'Database error' });
+	}
 });
 
 // updates existing todo
 // test with: curl -X PUT -H "Content-Type: application/json" -d '{"taskName": "updatedName", "taskInfo": "updatedInfo", "isCompleted": true, "deadline": "2023-11-23"}' "http://localhost:3000/api/todo/${TaskID}"
 app.put('/api/todo/:TaskID', async (req: Request, res: Response) => {
-  const { TaskID } = req.params;
-  const { taskName, taskInfo, isCompleted, deadline } = req.body;
+	const { TaskID } = req.params;
+	const { taskName, taskInfo, isCompleted, deadline } = req.body;
 
-  // Data validation
-  if (!taskName || typeof taskName !== 'string') {
-    return res.status(400).json({ error: 'Invalid input' });
-  }
+	// Create an array to hold SQL query parameters
+	let queryParams = [];
+	let query = 'UPDATE sys.Tasks SET ';
 
-  try {
-    const [result] = await dbPool.query(
-      'UPDATE sys.Tasks SET taskName = ?, taskInfo = ?, isCompleted = ?, deadline = ? WHERE TaskID = ?',
-      [taskName, taskInfo, isCompleted, deadline, TaskID]
-    );
-    const okPacket = result as OkPacket;
+	// Add different parts of the query and parameters based on what is provided in the body
+	if (taskName !== undefined) {
+		query += 'taskName = ?, ';
+		queryParams.push(taskName);
+	}
+	if (taskInfo !== undefined) {
+		query += 'taskInfo = ?, ';
+		queryParams.push(taskInfo);
+	}
+	if (isCompleted !== undefined) {
+		query += 'isCompleted = ?, ';
+		queryParams.push(isCompleted);
+	}
+	if (deadline !== undefined) {
+		query += 'deadline = ?, ';
+		queryParams.push(deadline);
+	}
 
-    if (okPacket.affectedRows === 0) {
-      return res.status(404).json({ error: 'Item not found' });
-    }
+	// Remove trailing comma and space, and add WHERE clause
+	query = query.slice(0, -2) + ' WHERE TaskID = ?';
+	queryParams.push(TaskID);
 
-    return res.status(200).json({ message: 'To-Do item updated', TaskID });
-  } catch (err) {
-    console.error(err);
-    return res.status(500).json({ error: 'Database error' });
-  }
+	// Check if any field was provided for update
+	if (queryParams.length === 1) {
+		return res.status(400).json({ error: 'No fields provided for update' });
+	}
+
+	try {
+		const [result] = await dbPool.query(query, queryParams);
+		const okPacket = result as OkPacket;
+
+		if (okPacket.affectedRows === 0) {
+			return res.status(404).json({ error: 'Item not found' });
+		}
+
+		return res.status(200).json({ message: 'To-Do item updated', TaskID });
+	} catch (err) {
+		console.error(err);
+		return res.status(500).json({ error: 'Database error' });
+	}
 });
+
 
 // Delete a specific todo item by TaskID
 // Test with: curl -X DELETE "http://localhost:3000/api/todo/${TaskID}"
 app.delete('/api/todo/:TaskID', async (req: Request, res: Response) => {
-  const { TaskID } = req.params;
+	const { TaskID } = req.params;
 
-  try {
-    const [result] = await dbPool.query('DELETE FROM sys.Tasks WHERE TaskID = ?', [TaskID]);
-    const okPacket = result as OkPacket;
+	try {
+		const [result] = await dbPool.query('DELETE FROM sys.Tasks WHERE TaskID = ?', [TaskID]);
+		const okPacket = result as OkPacket;
 
-    if (okPacket.affectedRows === 0) {
-      return res.status(404).json({ error: 'Item not found' });
-    }
+		if (okPacket.affectedRows === 0) {
+			return res.status(404).json({ error: 'Item not found' });
+		}
 
-    return res.status(200).json({ message: 'To-Do item deleted', TaskID });
-  } catch (err) {
-    console.error(err);
-    return res.status(500).json({ error: 'Database error' });
-  }
+		return res.status(200).json({ message: 'To-Do item deleted', TaskID });
+	} catch (err) {
+		console.error(err);
+		return res.status(500).json({ error: 'Database error' });
+	}
 });
 
 // Get tasks for a specific list
 app.get('/api/lists/:listId/tasks', async (req: Request, res: Response) => {
-  const { listId } = req.params;
-  try {
-    const [tasks] = await dbPool.query('SELECT * FROM sys.Tasks WHERE listId = ?', [listId]);
-    res.json(tasks);
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ error: 'Database error' });
-  }
+	const { listId } = req.params;
+	try {
+		const [tasks] = await dbPool.query('SELECT * FROM sys.Tasks WHERE listId = ?', [listId]);
+		res.json(tasks);
+	} catch (err) {
+		console.error(err);
+		res.status(500).json({ error: 'Database error' });
+	}
 });
 
 // Get all lists
 app.get('/api/lists', async (req: Request, res: Response) => {
-  try {
-    const [lists] = await dbPool.query('SELECT * FROM sys.Lists'); 
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ error: 'Database error' });
-  }
+	try {
+		const [lists] = await dbPool.query('SELECT * FROM sys.Lists');
+		res.json(lists);
+	} catch (err) {
+		console.error(err);
+		res.status(500).json({ error: 'Database error' });
+	}
 });
 
 
 
 
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+	console.log(`Server running on http://localhost:${PORT}`);
 });
--- src/server/public_html/main.ts
+++ src/server/public_html/main.ts
@@ -1,10 +1,14 @@
 // public_html/main.ts
 
+let currentListId: string | null = null;
+
 function setupListEventListeners() {
   document.querySelectorAll('#lists li').forEach(listItem => {
-    listItem.addEventListener('click', function(this: HTMLLIElement) {
+    listItem.addEventListener('click', function (this: HTMLLIElement) {
       const listId = this.getAttribute('data-list-id');
+      console.log("Clicked list ID:", listId);
       if (listId !== null) {
+        currentListId = listId;
         fetchTasksForList(listId);
       } else {
         console.error('List ID is null');
@@ -26,43 +30,203 @@ async function fetchTasksForList(listId: string) {
 
 function updateTaskListDisplay(tasks: any[]) {
   const tasksContainer = document.querySelector('#tasks');
-  if (tasksContainer != null){
-    tasksContainer.innerHTML = ''; 
+  if (tasksContainer != null) {
+    tasksContainer.innerHTML = '';
 
-    
     tasks.forEach(task => {
+      if (!task.isCompleted) {
         const taskItem = document.createElement('li');
-        taskItem.textContent = task.taskName; 
+
+        // Create a new element to hold the SVG
+        const svg = new DOMParser().parseFromString(`<svg class="checkIcon" fill="currentColor" width="20" height="20" viewBox="0 0 20 16" xmlns="http://www.w3.org/2000/svg" focusable="false"><path d="M10 3a7 7 0 100 14 7 7 0 000-14zm-8 7a8 8 0 1116 0 8 8 0 01-16 0z" fill="currentColor"></path></svg>`, 'image/svg+xml').documentElement;
+
+        // Append SVG to the task item
+        taskItem.appendChild(svg);
+        svg.addEventListener('click', () => updateTaskCompletion(task.taskId));
+
+        // Set the text content of the task item
+        taskItem.append(task.taskName);
+
+        // Append the task item to the tasks container
         tasksContainer.appendChild(taskItem);
+      }
     });
   }
 }
 
+
 // handles adding a single list item to the display
-function addListToDisplay(list: { listName: string | null; ListID: string }) {
+function addListToDisplay(list: { listName: string | null; listId: string }) {
+  const listsContainer = document.getElementById('lists');
+  const listItem = document.createElement('li');
+  if (list.listName !== null) listItem.innerText = list.listName;
+  listItem.setAttribute('data-list-id', list.listId);
+
+  //debugging
+  console.log("listID:", list.listId);
+  const listId = listItem.getAttribute('data-list-id');
+  console.log("Clicked list ID:", listId);
+
+  if (listsContainer != null)
+    listsContainer.appendChild(listItem);
+  setupListEventListeners();
+
+}
+
+// fetches and displays all lists
+async function fetchAndDisplayLists() {
+  try {
+    const response = await fetch('/api/lists');
+    const lists = await response.json();
     const listsContainer = document.getElementById('lists');
-    const listItem = document.createElement('li');
-    if (list.listName !== null) listItem.innerText = list.listName;
-    listItem.setAttribute('data-list-id', list.ListID);
-    if (listsContainer != null) 
-      listsContainer.appendChild(listItem);
-    setupListEventListeners(); 
-    
-  }
-  
-  // fetches and displays all lists
-  async function fetchAndDisplayLists() {
-    try {
-      const response = await fetch('/api/lists');
-      const lists = await response.json();
-      const listsContainer = document.getElementById('lists');
-      if (listsContainer != null) listsContainer.innerHTML = ''; 
-      lists.forEach(addListToDisplay); 
-    } catch (error) {
-      console.error("Failed to fetch lists:", error);
+    if (listsContainer != null) listsContainer.innerHTML = '';
+    lists.forEach(addListToDisplay);
+
+    // Check if there is at least one list and fetch tasks for the first one
+    if (lists.length > 0) {
+      currentListId = lists[0].listId;
+      if (currentListId !== null) {
+        fetchTasksForList(currentListId);
+      }
     }
+  } catch (error) {
+    console.error("Failed to fetch lists:", error);
   }
-  
+}
+
+async function updateTaskCompletion(taskId: string) {
+  if (!currentListId) {
+    console.error("No list selected");
+    return;
+  }
+
+  try {
+    const response = await fetch(`/api/todo/${taskId}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ isCompleted: true })  // Assuming other fields are handled on the server or are not required
+    });
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    // Fetch and update the tasks list after updating the task
+    fetchTasksForList(currentListId);
+  } catch (error) {
+    console.error("Failed to update task:", error);
+  }
+}
+
+
+// adds a task to current list
+async function addTask() {
+  if (currentListId === null) {
+    alert("Please select a list first!");
+    return;
+  }
+
+  const taskTextInput = document.getElementById('new-task-text') as HTMLInputElement;
+  const taskDeadlineInput = document.getElementById('task-deadline') as HTMLInputElement;
+
+  if (!taskTextInput || !taskDeadlineInput) {
+    console.error("Task text or deadline input not found");
+    return;
+  }
+
+  const taskText = taskTextInput.value;
+  const taskDeadline = taskDeadlineInput.value;
+
+  if (!taskText) {
+    alert("Please enter a task name.");
+    return;
+  }
+
+  // Prepare the task data
+  const taskData = {
+    taskName: taskText,
+    deadline: taskDeadline,
+    listId: currentListId
+  };
+
+  try {
+    const response = await fetch('/api/lists/' + currentListId + '/tasks', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(taskData)
+    });
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    // Clear the input fields after successful addition
+    taskTextInput.value = '';
+    taskDeadlineInput.value = '';
+
+    // Fetch and update the tasks list
+    fetchTasksForList(currentListId);
+
+  } catch (error) {
+    console.error("Failed to add task:", error);
+  }
+}
+
+// Function to add a new list
+async function addList() {
+  const listTextInput = document.getElementById('new-list-text') as HTMLInputElement;
+  if (!listTextInput) {
+    console.error("List text input not found");
+    return;
+  }
+
+  const listName = listTextInput.value;
+  if (!listName) {
+    alert("Please enter a list name.");
+    return;
+  }
+
+  try {
+    const response = await fetch('/api/lists', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ listName })
+    });
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    // Clear the input field after successful addition
+    listTextInput.value = '';
+
+    // Fetch and update the lists
+    fetchAndDisplayLists();
+
+  } catch (error) {
+    console.error("Failed to add list:", error);
+  }
+}
+
 
 window.addEventListener('load', fetchAndDisplayLists);
 
+const listButton = document.getElementById('listButton');
+if (listButton) {
+  listButton.addEventListener('click', addList);
+} else {
+  console.error("List button not found");
+}
+
+const taskButton = document.getElementById('taskButton');
+if (taskButton) {
+  taskButton.addEventListener('click', addTask);
+} else {
+  console.error("Task button not found");
+}
